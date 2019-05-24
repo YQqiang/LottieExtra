36198944c2ce0e30443d71f0c0b41481a1d4b0a2
yuqiang <1054572107@qq.com>
2019-05-24 16:51:46 +0800

    show hidden keypath

--- Example/LottieExtra/ViewController.swift
+++ Example/LottieExtra/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import LottieExtra
 
 class ViewController: UIViewController {
+    
+    fileprivate lazy var lottieView: LottieExtraView = {
+        return LottieExtraView()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(lottieView)
+        lottieView.translatesAutoresizingMaskIntoConstraints = false
+        lottieView.topAnchor.constraint(equalTo: view.topAnchor, constant: 32).isActive = true
+        lottieView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        lottieView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        lottieView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        lottieView.configAnimation(name: "micro")
+        lottieView.play()
+        lottieView.animationView.loopMode = .loop
+        lottieView.hidden(keyPath: "能量_电池to用电")
+        lottieView.hidden(keyPath: "能量_电池to电网")
+        lottieView.hidden(keyPath: "能量_电网to电池")
+        lottieView.hidden(keyPath: "能量_电网to用电")
+
+        lottieView.hidden(keyPath: "能量_逆变器to用电")
+        lottieView.hidden(keyPath: "能量_逆变器to电网")
+        lottieView.hidden(keyPath: "能量_逆变器to电池")
+
+        lottieView.hidden(keyPath: "按钮_电网")
+        
+        lottieView.show(keyPath: "**")
+        
+        lottieView.animationView.logHierarchyKeypaths()
+//
+        lottieView.show(keyPath: "能量_逆变器to用电")
+//        lottieView.show(keyPath: "能量_逆变器to电网")
+        
+//        lottieView.hidden(keyPath: "能量_逆变器to电池")
+//        lottieView.show(keyPath: "*")
     }
 
     override func didReceiveMemoryWarning() {
---
+++ LottieExtra/Classes/LottieExtraView.swift
@@ -0,0 +1,95 @@
+//
+//  LottieExtraView.swift
+//  LottieExtra
+//
+//  Created by sungrow on 2019/5/24.
+//
+
+import UIKit
+import Lottie
+
+@objcMembers open class LottieExtraView: UIView {
+
+    public private(set) var animationView: AnimationView = {
+        let animationV = AnimationView()
+        animationV.contentMode = .scaleAspectFit
+        return animationV
+    }()
+    
+    fileprivate lazy var kps: [FloatValueProvider] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createView()
+    }
+    
+    private func createView() {
+        addSubview(animationView)
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        animationView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        animationView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        animationView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+}
+
+extension LottieExtraView {
+    @objc public func configAnimation(name: String, bundle: Bundle = Bundle.main) {
+        let animation = Animation.named(name, bundle: bundle, subdirectory: nil, animationCache: nil)
+        animationView.animation = animation
+    }
+}
+
+// MARK: - control
+extension LottieExtraView {
+    
+    @objc public func play() {
+        animationView.play()
+    }
+    
+    @objc public func pause() {
+        animationView.pause()
+    }
+    
+    @objc public func stop() {
+        animationView.stop()
+    }
+}
+
+// MARK: - KeyPath Transform Opacity
+extension LottieExtraView {
+    
+    @objc public func show(keyPath: String) {
+        show(keyPaths: [keyPath])
+    }
+    
+    @objc public func show(keyPaths: [String]) {
+        configKeyPath(keyPaths, opacity: 100.0)
+    }
+    
+    @objc public func hidden(keyPath: String) {
+        hidden(keyPaths: [keyPath])
+    }
+    
+    @objc public func hidden(keyPaths: [String]) {
+        configKeyPath(keyPaths, opacity: 0.0)
+    }
+    
+    @objc public func configKeyPath(_ keyPaths: [String], opacity: CGFloat) {
+        keyPaths.forEach { (keyPath) in
+            let kp = AnimationKeypath(keypath: "\(keyPath).Transform.Opacity")
+            let provider = FloatValueProvider(opacity)
+            animationView.setValueProvider(provider, keypath: kp)
+            kps.append(provider)
+        }
+    }
+}
