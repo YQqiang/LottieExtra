d432a3f4563276051fe9b277f1dbbc4461b6c7ab
yuqiang <1054572107@qq.com>
2019-05-25 13:34:23 +0800

    增加获取位置，锚点，不透明度等方法

---
+++ LottieExtra/Classes/AnimationViewExtra.swift
@@ -0,0 +1,59 @@
+//
+//  AnimationViewExtra.swift
+//  LottieExtra
+//
+//  Created by sungrow on 2019/5/25.
+//
+
+import Lottie
+
+public extension AnimationView {
+    
+    /// 获取指定keyPath的位置
+    ///
+    /// - Parameter keyPath: keyPath
+    /// - Returns: 位置
+    @objc func position(for keyPath: String) -> CGPoint {
+        let kp = AnimationKeypath(keypath: "\(keyPath).Transform.Position")
+        guard let vector = getValue(for: kp, atFrame: nil) as? Vector3D  else {
+            return CGPoint.zero
+        }
+        return CGPoint(x: vector.sizeValue.width, y: vector.sizeValue.height)
+    }
+    
+    /// 获取指定keyPath的锚点
+    ///
+    /// - Parameter keyPath: keyPath
+    /// - Returns: 锚点
+    @objc func anchorPoint(for keyPath: String) -> CGPoint {
+        let kp = AnimationKeypath(keypath: "\(keyPath).Transform.Anchor Point")
+        guard let vector = getValue(for: kp, atFrame: nil) as? Vector3D  else {
+            return CGPoint.zero
+        }
+        return CGPoint(x: vector.sizeValue.width, y: vector.sizeValue.height)
+    }
+    
+    /// 获取指定keyPath的旋转角度
+    ///
+    /// - Parameter keyPath: keyPath
+    /// - Returns: 旋转角度
+    @objc func rotation(for keyPath: String) -> Double {
+        let kp = AnimationKeypath(keypath: "\(keyPath).Transform.Rotation")
+        guard let vector = getValue(for: kp, atFrame: nil) as? Vector1D  else {
+            return Double.zero
+        }
+        return Mirror(reflecting: vector).children.first?.value as? Double ?? Double.zero
+    }
+    
+    /// 获取指定keyPath的不透明度
+    ///
+    /// - Parameter keyPath: keyPath
+    /// - Returns: 不透明度
+    @objc func opacity(for keyPath: String) -> Double {
+        let kp = AnimationKeypath(keypath: "\(keyPath).Transform.Opacity")
+        guard let vector = getValue(for: kp, atFrame: nil) as? Vector1D  else {
+            return Double.zero
+        }
+        return Mirror(reflecting: vector).children.first?.value as? Double ?? Double.zero
+    }
+}
